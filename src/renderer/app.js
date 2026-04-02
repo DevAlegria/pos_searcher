58e62ecf6c75e6481b4bc472b218a48ee41e9022
DevAlegria <yhonier.alegria@gmail.com>
2026-04-01 19:58:24 -0500

    Refactor data loading in cargarDatos function: replace static product list with API call to fetch products

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -3,7 +3,7 @@ const inputBusqueda = document.getElementById('busqueda');
 const tablaResultados = document.getElementById('resultados');
 
 async function cargarDatos(term = '') {
-  const productsList = [];
+  const productsList = await window.api.searchProducts(term);
   const productsArray = getProductsArray(productsList);
   
   tablaResultados.innerHTML = productsArray.map(product => productItem(product)).join('');
@@ -24,103 +24,6 @@ async function cargarDatos(term = '') {
 function getProductsArray(products) {
   const mapProducts = new Map();
 
-// 🟢 1. Solo en local (con stock)
-mapProducts.set('P001', {
-  reference: 'P001',
-  description: 'Bujía NGK',
-  price: 15,
-  quantity: 10,
-  location: 'MOSTRADOR',
-  storage: null
-});
-
-// 🟢 2. Solo en local (sin stock)
-mapProducts.set('P002', {
-  reference: 'P002',
-  description: 'Filtro de aire',
-  price: 20,
-  quantity: 0,
-  location: 'ESTANTE A',
-  storage: null
-});
-
-// 🔵 3. Solo en bodega (con stock)
-mapProducts.set('P003', {
-  reference: 'P003',
-  description: 'Kit arrastre',
-  price: 120,
-  quantity: null,
-  location: null,
-  storage: {
-    location: 'BODEGA',
-    quantity: 8
-  }
-});
-
-// 🔵 4. Solo en bodega (sin stock)
-mapProducts.set('P004', {
-  reference: 'P004',
-  description: 'Pastillas de freno',
-  price: 35,
-  quantity: null,
-  location: null,
-  storage: {
-    location: 'zx-004',
-    quantity: 0
-  }
-});
-
-// 🟡 5. Local con stock, bodega sin stock
-mapProducts.set('P005', {
-  reference: 'P005',
-  description: 'Aceite 4T',
-  price: 25,
-  quantity: 6,
-  location: 'MOSTRADOR',
-  storage: {
-    location: 'BODEGA',
-    quantity: 0
-  }
-});
-
-// 🟡 6. Local sin stock, bodega con stock
-mapProducts.set('P006', {
-  reference: 'P006',
-  description: 'Cadena',
-  price: 80,
-  quantity: 0,
-  location: 'ESTANTE B',
-  storage: {
-    location: 'BODEGA',
-    quantity: 12
-  }
-});
-
-// 🟡 7. Local y bodega con stock
-mapProducts.set('P007', {
-  reference: 'P007',
-  description: 'Disco de freno',
-  price: 150,
-  quantity: 3,
-  location: 'VITRINA',
-  storage: {
-    location: 'BODEGA',
-    quantity: 5
-  }
-});
-
-// 🔴 8. Sin stock en ningún lado
-mapProducts.set('P008', {
-  reference: 'P008',
-  description: 'Guaya acelerador',
-  price: 18,
-  quantity: 0,
-  location: 'ESTANTE C',
-  storage: {
-    location: 'BODEGA',
-    quantity: 0
-  }
-})
 products.forEach(
     product => {
       const referenceStartsWithZ = product.strReferencia.startsWith('Z-');
